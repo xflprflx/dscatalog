a9db0b22bedc3e1ed28f043d43064d4dacd4ab2e
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-07-06 20:31:29 -0300

    Home styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,14 +1,11 @@
-import Navbar from 'components/Navbar';
 import './assets/styles/custom.scss';
 import './App.css';
+import Home from 'pages/Home';
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <h1>Hello DSCatalog</h1>
-    </>
+    <Home />
   );
-}
+};
 
 export default App;
--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
       <div className="container-fluid">
-        <a href="link" className="nav-logo-text">
+        <a href="link" className="nav-logo-text">   
           <h4>DS Catalog</h4>
         </a>
         <button
