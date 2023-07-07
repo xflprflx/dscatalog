df703b3a90da2afd96ae357c81f9431b55f23342
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-07-07 14:41:53 -0300

    Routes

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,11 +1,10 @@
 import './assets/styles/custom.scss';
 import './App.css';
-import Home from 'pages/Home';
+
+import Routes from 'Routes';
 
 const App = () => {
-  return (
-    <Home />
-  );
+  return <Routes />;
 };
 
 export default App;
