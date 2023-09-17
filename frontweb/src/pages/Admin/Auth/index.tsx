93220bc9d4c5727ccb33fb63d94b87d67e1a8593
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-09-17 20:28:14 -0300

    Banner styling

---
+++ frontweb/src/pages/Admin/Auth/index.tsx
@@ -0,0 +1,33 @@
+import { ReactComponent as AuthImage } from 'assets/images/auth-image.svg';
+import { Route, Switch } from 'react-router-dom';
+
+import './styles.css'
+
+
+const Auth = () => {
+  return (
+    <div className="auth-container">
+        <div className="auth-banner-container">
+            <h1>Divulgue seu produtos<br/> no DS Catalog</h1>
+            <p>Faça parte do nosso catálog de divulgação e aumente a venda dos seus produtos</p>
+            <AuthImage/>
+        </div>
+        <div className='auth-form-container'>
+            <Switch>
+                <Route path="/admin/auth/login">
+                    <h1>Card de login</h1>
+                </Route>
+                <Route path="/admin/auth/signup">
+                    <h1>Card de signup</h1>
+                </Route>
+                <Route path="/admin/auth/recover">
+                    <h1>Card de recover</h1>
+                </Route>
+            </Switch>
+
+        </div>
+    </div>
+  );
+};
+
+export default Auth;
