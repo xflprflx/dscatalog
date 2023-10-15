c53faa18ecf87715a688cd88eab281fe1ad506c6
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-10-15 09:52:19 -0300

    hasAnyRoles function

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -109,3 +109,15 @@ export const isAuthenticated = () : boolean => {
   const tokenData = getTokenData();
   return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
 }
+
+export const hasAnyRoles = (roles: Role[]) : boolean => {
+  if(roles.length === 0){
+    return true;
+  }
+  
+  const tokenData = getTokenData();
+  if(tokenData !== undefined){
+    return roles.some(role => tokenData.authorities.includes(role))
+  }
+  return false;
+}
